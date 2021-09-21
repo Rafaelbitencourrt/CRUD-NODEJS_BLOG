021ca4a52c3cf5f3782d54582eb9a2d752da2b97
Rafael <rafaelbitencourrt@gmail.com>
2021-09-21 10:55:25 -0300

    ok

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const { Sequelize } = require("sequelize/types");
 const app = express();
 const PORT = 3000;
+const connection = require("./database/database");
 
 //VIEW ENGINE
 app.set("view engine", "ejs");
@@ -13,6 +15,17 @@ app.use(express.json());
 //STATIC
 
 app.use(express.static("public"));
+
+//DATABASE
+
+connection
+  .authenticate()
+  .then(() => {
+    console.log("Banco de dados conectado com sucesso!");
+  })
+  .catch((error) => {
+    console.log("não conectado", error);
+  });
 
 app.get("/", (req, res) => {
   res.render("index");
