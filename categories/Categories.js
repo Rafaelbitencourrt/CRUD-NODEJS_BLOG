d8199f366405a5dc3a6c74bf927c5eb3c32a51c2
Rafael <rafaelbitencourrt@gmail.com>
2021-09-27 12:08:19 -0300

    ok

--- categories/Categories.js
+++ categories/Categories.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 const connection = require("../database/database");
 
-const Category = connection.define("categorias", {
+const Category = connection.define("categories", {
   title: {
     type: Sequelize.STRING,
     allowNull: false,
