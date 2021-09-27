aa99a0977c2616596f4c7a1742fe2aba01ac8f35
Rafael <rafaelbitencourrt@gmail.com>
2021-09-27 07:13:06 -0300

    criação e formatação do textearea

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../categories/Categories");
 
 router.get("/articles", (req, res) => {
-  res.send("Rota para artigos do admin");
+  res.send("Rota artigos");
 });
 
 router.get("/admin/articles/new", (req, res) => {
-  res.render("admin/articles/new");
+  Category.findAll().then((categories) => {
+    res.render("admin/articles/new", { categories: categories });
+  });
 });
 
 module.exports = router;
