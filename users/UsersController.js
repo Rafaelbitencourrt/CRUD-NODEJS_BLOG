429fa831d53da6a060ff9130bc715a89137fe515
Rafael <rafaelbitencourrt@gmail.com>
2021-11-06 08:00:12 -0300

    ok

--- users/UsersController.js
+++ users/UsersController.js
@@ -92,4 +92,9 @@ router.post("/authenticate", (req, res) => {
   });
 });
 
+router.get("logout", (req, res) => {
+  req.session.user = undefined;
+  res.redirect("/");
+});
+
 module.exports = router;
