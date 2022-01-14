41af19b8cc599151bce0ffe01d8b52f5d4a2f2ed
Rafael <rafaelbitencourrt@gmail.com>
2022-01-14 08:10:08 -0300

    up date database

--- database/database.js
+++ database/database.js
@@ -1,9 +1,12 @@
 const Sequelize = require("sequelize");
 
 const connection = new Sequelize("blog", "root", "28461973", {
-  host: "localhost",
+  host: "127.0.0.1",
   dialect: "mysql",
   timezone: "-03:00",
+  port: "3306",
+  connectionLimit: 10,
+  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
 });
 
 module.exports = connection;
